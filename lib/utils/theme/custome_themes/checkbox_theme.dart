5eb82f14ea05fa8a9d5a0fb33bcf9ad762813d29
menintel <menintel@tuta.io>
2025-04-22 18:32:21 +0100

    created themes for App

---
+++ lib/utils/theme/custome_themes/checkbox_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// Custom class for light & Dark Text Themes
+class TCheckboxTheme {
+  TCheckboxTheme._();
+
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white; // Return a color when the checkbox is selected
+      }else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue; // Return a color when the checkbox is selected
+      }else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white; // Return a color when the checkbox is selected
+      }else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue; // Return a color when the checkbox is selected
+      }else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+}
